f7e7a67053be4b21ec464832e6c7a88e9ff7cfac
JoaoPauloAscari <joaoascari@hotmail.com>
2021-06-20 12:18:57 -0300

    20/06/2021
    Update of data source

--- readdata.R
+++ readdata.R
@@ -18,9 +18,9 @@ Sys.setlocale("LC_ALL", "pt_BR.UTF-8")
 
 
 
-#DelPonte Lab
-delponte = gsheet2tbl("https://docs.google.com/spreadsheets/d/1R6UXFcsbVgpPYyvfkPvZ81rqkyk4U_9aS7qV8TKPsxQ/edit?usp=sharing")
-delponte_all <- delponte %>%
+#Ascari et al 202?
+ascari = gsheet2tbl("https://docs.google.com/spreadsheets/d/1R6UXFcsbVgpPYyvfkPvZ81rqkyk4U_9aS7qV8TKPsxQ/edit#gid=532754791")
+ascari_all <- ascari %>%
   filter(labcode != 0) %>%
   mutate(id = case_when(
     is.na(py_binomial) ~ "No",
@@ -29,52 +29,15 @@ delponte_all <- delponte %>%
   mutate(wheat = case_when(
     host_binomial == "Triticum aestivum" ~ "Wheat",
     TRUE ~ "Non-wheat"
-  )) %>% 
-  mutate(Source = "Ascari et al. (unpublished)")
+  ))
 
-delponte_all = delponte_all %>% 
-  dplyr::select(Source, labcode, year, lat, lon, country, state_province, county_municipality, collector, host_binomial, py_binomial, py_lineage, wheat)
-
-
-#Kamoun lab
-kamoun = gsheet2tbl("https://docs.google.com/spreadsheets/d/13PyoQobtdE3xessW14rHud45MlXUOsMOgKDWG81kxCc/edit#gid=1082341206")
-kamoun_all = kamoun %>%
-  filter(labcode != 0) %>%
-  mutate(id = case_when(
-    is.na(py_binomial) ~ "No",
-    TRUE ~ "Yes"
-  )) %>%
-  mutate(wheat = case_when(
-    host_binomial == "Triticum aestivum" ~ "Wheat",
-    TRUE ~ "Non-wheat"
-  )) %>% 
-  mutate(Source = "Kamoun-Lab")
-
-kamoun_all <- kamoun_all %>% 
-  dplyr::select(Source, labcode, year, lat, lon, country, state_province, county_municipality, collector, host_binomial, py_binomial, py_lineage, wheat)
-
-
-#Farman lab
-farman = gsheet2tbl("https://docs.google.com/spreadsheets/d/1Ax1QWcVsNBxrRGXLxhwlvjwZtDjixdsuyVC51-0bgs0/edit?usp=sharing")
-farman_all = farman %>%
-  filter(labcode != 0) %>%
-  mutate(id = case_when(
-    is.na(py_binomial) ~ "No",
-    TRUE ~ "Yes"
-  )) %>%
-  mutate(wheat = case_when(
-    host_binomial == "Triticum aestivum" ~ "Wheat",
-    TRUE ~ "Non-wheat"
-  )) %>% 
-  mutate(Source = "Farman-Lab")
-
-farman_all <- farman_all %>% 
-  dplyr::select(Source, labcode, year, lat, lon, country, state_province, county_municipality, collector, host_binomial, py_binomial, py_lineage, wheat)
+ascari_all = ascari_all %>% 
+  dplyr::select(labcode, year, lat, lon, country, state_province, county_municipality, source, host_binomial, py_binomial, py_lineage, wheat)
 
 
 
-#BioTrigo 
-biotrigo = gsheet2tbl("https://docs.google.com/spreadsheets/d/18Ifv9rKzHM4do5foH3ZPsmXkZBGqnAORpFoXt0gdWjg/edit?usp=sharing")
+#BioTrigo
+biotrigo = gsheet2tbl("https://docs.google.com/spreadsheets/d/1R6UXFcsbVgpPYyvfkPvZ81rqkyk4U_9aS7qV8TKPsxQ/edit#gid=1498154130")
 biotrigo_all = biotrigo %>%
   filter(labcode != 0) %>%
   mutate(id = case_when(
@@ -84,16 +47,16 @@ biotrigo_all = biotrigo %>%
   mutate(wheat = case_when(
     host_binomial == "Triticum aestivum" ~ "Wheat",
     TRUE ~ "Non-wheat"
-  )) %>% 
-  mutate(Source = "BioTrigo Genetica")
+  ))
 
-biotrigo_all <- biotrigo_all %>% 
-  dplyr::select(Source, labcode, year, lat, lon, country, state_province, county_municipality, collector, host_binomial, py_binomial, py_lineage, wheat)
+biotrigo_all <- biotrigo_all %>%
+  dplyr::select(labcode, year, lat, lon, country, state_province, county_municipality, source, host_binomial, py_binomial, py_lineage, wheat)
 
 
-#Croll lab 
-croll = gsheet2tbl("https://docs.google.com/spreadsheets/d/1uJGZSdtZJc0mHlFB7yoSeWTs2zMVzlSrOaCNCRp1Il0/edit?usp=sharing")
-croll_all = croll %>%
+
+#Tembo et al 2021
+tembo = gsheet2tbl("https://docs.google.com/spreadsheets/d/1R6UXFcsbVgpPYyvfkPvZ81rqkyk4U_9aS7qV8TKPsxQ/edit#gid=1727645447")
+tembo_all = tembo %>%
   filter(labcode != 0) %>%
   mutate(id = case_when(
     is.na(py_binomial) ~ "No",
@@ -102,11 +65,67 @@ croll_all = croll %>%
   mutate(wheat = case_when(
     host_binomial == "Triticum aestivum" ~ "Wheat",
     TRUE ~ "Non-wheat"
-  )) %>% 
-  mutate(Source = "https://doi.org/10.5281/zenodo.4655959")
+  ))
 
-croll_all <- croll_all %>% 
-  dplyr::select(Source, labcode, year, lat, lon, country, state_province, county_municipality, host_binomial, py_binomial, py_lineage, wheat)
+tembo_all <- tembo_all %>% 
+  dplyr::select(labcode, year, lat, lon, country, state_province, county_municipality, source, host_binomial, py_binomial, py_lineage, wheat)
+
+
+
+# #Croll 2021 
+# croll = gsheet2tbl("https://docs.google.com/spreadsheets/d/1R6UXFcsbVgpPYyvfkPvZ81rqkyk4U_9aS7qV8TKPsxQ/edit#gid=106318963")
+# croll_all = croll %>%
+#   filter(labcode != 0) %>%
+#   mutate(id = case_when(
+#     is.na(py_binomial) ~ "No",
+#     TRUE ~ "Yes"
+#   )) %>%
+#   mutate(wheat = case_when(
+#     host_binomial == "Triticum aestivum" ~ "Wheat",
+#     TRUE ~ "Non-wheat"
+#   ))
+# 
+# croll_all <- croll_all %>% 
+#   dplyr::select(labcode, year, lat, lon, country, state_province, county_municipality, source, host_binomial, py_binomial, py_lineage, wheat)
+# 
+
+
+
+
+# #Rahnama et al 202?
+# Rahnama = gsheet2tbl("https://docs.google.com/spreadsheets/d/1R6UXFcsbVgpPYyvfkPvZ81rqkyk4U_9aS7qV8TKPsxQ/edit#gid=2126271613")
+# Rahnama_all = Rahnama %>%
+#   filter(labcode != 0) %>%
+#   mutate(id = case_when(
+#     is.na(py_binomial) ~ "No",
+#     TRUE ~ "Yes"
+#   )) %>%
+#   mutate(wheat = case_when(
+#     host_binomial == "Triticum aestivum" ~ "Wheat",
+#     TRUE ~ "Non-wheat"
+#   ))
+# 
+# Rahnama_all <- Rahnama_all %>% 
+#   dplyr::select(labcode, year, lat, lon, country, state_province, county_municipality, source, host_binomial, py_binomial, py_lineage, wheat)
+
+
+
+
+# #Castroaugudin et al 2016
+# Castroaugudin = gsheet2tbl("https://docs.google.com/spreadsheets/d/1R6UXFcsbVgpPYyvfkPvZ81rqkyk4U_9aS7qV8TKPsxQ/edit#gid=1870344414")
+# Castroaugudin_all = Castroaugudin %>%
+#   filter(labcode != 0) %>%
+#   mutate(id = case_when(
+#     is.na(py_binomial) ~ "No",
+#     TRUE ~ "Yes"
+#   )) %>%
+#   mutate(wheat = case_when(
+#     host_binomial == "Triticum aestivum" ~ "Wheat",
+#     TRUE ~ "Non-wheat"
+#   ))
+# 
+# Castroaugudin_all <- Castroaugudin_all %>% 
+#   dplyr::select(labcode, year, lat, lon, country, state_province, county_municipality, source, host_binomial, py_binomial, py_lineage, wheat)
 
 
 
@@ -114,8 +133,7 @@ croll_all <- croll_all %>%
 
 
 #Raw data
-wb_all = bind_rows(delponte_all, kamoun_all, farman_all, biotrigo_all, croll_all)
-
+wb_all = bind_rows(ascari_all, biotrigo_all, tembo_all)
 
 
 
@@ -124,7 +142,6 @@ wb_all = bind_rows(delponte_all, kamoun_all, farman_all, biotrigo_all, croll_all
 sd <- SharedData$new(wb_all)
 
 wb_all2 <- wb_all %>% 
-  select(lab, labcode, year, country, collector, host_binomial,
-         py_binomial, py_lineage, wheat)
+  select(labcode, year, country, source, host_binomial, py_binomial, py_lineage, wheat)
   
 sd2 <- SharedData$new(wb_all2)
